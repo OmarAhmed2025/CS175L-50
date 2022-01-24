c360fcd2a2a94fe812200cbe2231bfb8ebbb7509
OmarAhmed2025 <91150797+OmarAhmed2025@users.noreply.github.com>
2022-01-24 18:54:15 -0500

    myInfo version 2

--- myInfo.py
+++ myInfo.py
@@ -2,5 +2,8 @@
 #Omar Ahmed
 #myInfo
 print("My name is Omar Ahmed")
-print("I am a Student")
+print("Address: 76 Chruchill Downs Sreet, Tinton Falls, Nj 07724")
+print(" Telephone Number: 732-443-6565")
+print("Major: Computer Science")
+
 
