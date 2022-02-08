1dd39b6d6d746186fa624e816fcaef8572551a46
OmarAhmed2025 <91150797+OmarAhmed2025@users.noreply.github.com>
2022-02-07 19:03:34 -0500

    Restaurant

---
+++ Resturant.py
@@ -0,0 +1,35 @@
+#Omar Ahmed
+# CS175-50L
+#Resturant
+#SP 2022
+
+# Initial Booleans Values
+
+vegetarian=False
+vegan=False
+glutenfree=False
+
+#Prompting User
+
+vege= input("Is anyone in your party a vegetrian?: ")
+if vege == 'yes':
+    vegetarian= True
+
+veg=input("Is anyone is your party vegan?: ")
+if veg == 'yes':
+    vegan = True
+
+gluten=input("Is anyone in your party gluten-free?: ")
+if gluten == 'yes':
+    glutenfree= True
+
+#Output of Resturant Choices 
+
+if vegan:
+    print('Corner Cafe \n The Chefs kitchen')
+elif glutenfree:
+    print('Main Street Pizza Company \n Corner Cafe \n The Chefs Kitchen ')
+elif vegetarian:
+    print('Main Street Pizza Company \n Corner Cafe \n The Chefs Kitchen \n Mamas Fine Italian')
+else:
+    print('Joes Gourmet Burgers \nMain Street Pizza Company \n Corner Cafe \n The Chefs Kitchen \n Mamas Fine Italian')
